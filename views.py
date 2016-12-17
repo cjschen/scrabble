3739b4bfd0237f12ad7f077708bcc3dcddc44ff4
Sijia Chen <sijia1996@gmail.com>
2016-12-17 17:11:00 -0500

    flask server works

--- views.py
+++ views.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route("/randomize", methods=["GET", "POST"])
 def randomize():
     return "Hello World!"
+
+@app.route("/")
+def index():
+    return render_template("index.html")
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
